584289456f4b04ded8a582e8a92218ffeb555c00
BetyDobre <bettydobre@gmail.com>
2022-06-13 12:06:53 +0300

    added eureka server base project

---
+++ eureka-namingserver/eureka-namingserver/src/main/java/com/awbd/eurekanamingserver/EurekaNamingserverApplication.java
@@ -0,0 +1,13 @@
+package com.awbd.eurekanamingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaNamingserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaNamingserverApplication.class, args);
+	}
+
+}
