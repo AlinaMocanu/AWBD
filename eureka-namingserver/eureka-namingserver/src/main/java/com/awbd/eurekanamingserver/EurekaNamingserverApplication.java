3ebf95b721578083c0f3e3354ff909edba6a30e6
Alina Mocanu <alinamocanu2@gmail.com>
2022-06-17 18:33:10 +0300

    add eureka server

--- eureka-namingserver/eureka-namingserver/src/main/java/com/awbd/eurekanamingserver/EurekaNamingserverApplication.java
+++ eureka-namingserver/eureka-namingserver/src/main/java/com/awbd/eurekanamingserver/EurekaNamingserverApplication.java
@@ -2,8 +2,10 @@ package com.awbd.eurekanamingserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaNamingserverApplication {
 
 	public static void main(String[] args) {
